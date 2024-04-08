35d29196448705457686692ee9d5a1a4610fd5e6
Tony Galmiche <tony.galmiche@infosaone.com>
2024-04-08 14:16:40 +0200

    modifs

--- models/is_doc_moule.py
+++ models/is_doc_moule.py
@@ -105,6 +105,39 @@ class IsDocMoule(models.Model):
             }
 
 
+    def acceder_doc_action(self):
+        for obj in self:
+            print(obj)
+
+            res= {
+                'name': 'Doc',
+                'view_mode': 'form',
+                'res_model': 'is.doc.moule',
+                'res_id': obj.id,
+                #'view_id': view_id.id,
+                'type': 'ir.actions.act_window',
+            }
+            return res
+
+
+            # return {
+            #         'name': obj.name,
+            #         'view_mode': 'form,tree',
+            #         "views"    : [
+            #             (gantt_id, "dhtmlxgantt_project"),
+            #             (tree_id, "tree"),
+            #             (False, "form"),(False, "kanban"),(False, "calendar"),(False, "pivot"),(False, "graph")],
+            #         'res_model': 'is.doc.moule',
+            #         'domain': [
+            #             ('id','in',ids),
+            #         ],
+            #         'type': 'ir.actions.act_window',
+            #         "context": ctx,
+            #         'limit': 1000,
+            #     }
+
+
+
     def list_doc(self,obj,ids, view_mode=False):
         if not view_mode:
             return False
@@ -155,11 +188,12 @@ class IsDocMoule(models.Model):
 
     def doc_projet_action(self):
         for obj in self:
-            docs=self.env['is.doc.moule'].search([ ('idproject', '=', obj.idproject.id) ])
-            ids=[]
-            for doc in docs:
-                ids.append(doc.id)
-            return obj.list_doc(obj.idproject,ids)
+            return obj.idproject.gantt_action()
+            # docs=self.env['is.doc.moule'].search([ ('idproject', '=', obj.idproject.id) ])
+            # ids=[]
+            # for doc in docs:
+            #     ids.append(doc.id)
+            # return obj.list_doc(obj.idproject,ids)
 
 
     @api.model
@@ -309,6 +343,42 @@ class is_dossierf(models.Model):
             ctx={
                 'default_type_document': 'Dossier F',
                 'default_dossierf_id'  : obj.id,
+                'default_etat'         :'AF',
+                'default_dateend'      : datetime.today(),
+                'default_idresp'       : self._uid,
+            }
+            return {
+                'name': obj.name,
+                'view_mode': 'dhtmlxgantt_project,tree,form,kanban,calendar,pivot,graph',
+                "views"    : [
+                    (gantt_id, "dhtmlxgantt_project"),
+                    (tree_id, "tree"),
+                    (False, "form"),(False, "kanban"),(False, "calendar"),(False, "pivot"),(False, "graph")],
+                'res_model': 'is.doc.moule',
+                'domain': [
+                    ('id','in',ids),
+                ],
+                'type': 'ir.actions.act_window',
+                "context": ctx,
+                'limit': 1000,
+            }
+        
+
+
+class is_mold_project(models.Model):
+    _inherit = 'is.mold.project'
+
+    def gantt_action(self):
+        for obj in self:
+            docs=self.env['is.doc.moule'].search([ ('idproject', '=', obj.id) ])
+            ids=[]
+            for doc in docs:
+                ids.append(doc.id)
+            tree_id  = self.env.ref('is_dynacase2odoo.is_doc_moule_edit_tree_view').id
+            gantt_id = self.env.ref('is_dynacase2odoo.is_doc_moule_moule_dhtmlxgantt_project_view').id
+            ctx={
+                #'default_type_document': 'Moule',
+                #'default_dossierf_id'  : obj.id,
                 'default_etat'         :'AF',
                 'default_dateend'      : datetime.today(),
                 'default_idresp'       : self._uid,
