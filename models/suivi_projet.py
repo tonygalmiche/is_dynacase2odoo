5cba089408f2e45b373e0860b661d42d79ead170
Tony Galmiche <tony.galmiche@infosaone.com>
2024-10-02 18:27:05 +0200

    modifs

--- models/suivi_projet.py
+++ models/suivi_projet.py
@@ -11,12 +11,12 @@ class IsDocMoule(models.Model):
 
     def get_suivi_projet(
             self, 
-            cp=False,
+            cp_id=False,
             client=False,
             projet=False,
             moule=False,
             type_moule=False,
-            modele=False,
+            modele_id=False,
             ok=False
         ):
         cr = self._cr
@@ -24,92 +24,166 @@ class IsDocMoule(models.Model):
         #** set/get var *****************************************************
         debut=datetime.now()
         if ok:
-            self.env['is.mem.var'].set(self._uid, 'suivi_projet_cp'        , cp)
+            self.env['is.mem.var'].set(self._uid, 'suivi_projet_cp_id'     , cp_id)
             self.env['is.mem.var'].set(self._uid, 'suivi_projet_client'    , client)
             self.env['is.mem.var'].set(self._uid, 'suivi_projet_projet'    , projet)
             self.env['is.mem.var'].set(self._uid, 'suivi_projet_moule'     , moule)
             self.env['is.mem.var'].set(self._uid, 'suivi_projet_type_moule', type_moule)
-            self.env['is.mem.var'].set(self._uid, 'suivi_projet_modele'    , modele)
+            self.env['is.mem.var'].set(self._uid, 'suivi_projet_modele_id' , modele_id)
         else:
-            cp         = self.env['is.mem.var'].get(self._uid, 'suivi_projet_cp')
+            cp_id      = self.env['is.mem.var'].get(self._uid, 'suivi_projet_cp_id')
             client     = self.env['is.mem.var'].get(self._uid, 'suivi_projet_client')
             projet     = self.env['is.mem.var'].get(self._uid, 'suivi_projet_projet')
             moule      = self.env['is.mem.var'].get(self._uid, 'suivi_projet_moule')
             type_moule = self.env['is.mem.var'].get(self._uid, 'suivi_projet_type_moule')
-            modele     = self.env['is.mem.var'].get(self._uid, 'suivi_projet_modele')
+            modele_id  = self.env['is.mem.var'].get(self._uid, 'suivi_projet_modele_id')
         _logger.info("set/get var (durée=%.2fs)"%(datetime.now()-debut).total_seconds())
 
 
-
-        SQL="""
-            select
-                idm.id,
-                im.name         moule,
-                idm.idmoule,
-                im.project,
-                imp.name        projet,
-                imp.chef_projet_id,
-                rp.name         cp,
-                imp.client_id,
-                client.name     client,
-                ipp.ppr_famille famille,
-                idm.param_project_id
-            from is_doc_moule idm inner join is_mold im           on idm.idmoule=im.id
-                                inner join is_mold_project imp  on im.project=imp.id
-                                inner join res_users ru         on imp.chef_projet_id=ru.id
-                                inner join res_partner rp       on ru.partner_id=rp.id
-                                inner join res_partner client   on imp.client_id=client.id
-                                inner join is_param_project ipp on idm.param_project_id=ipp.id
-            where idm.idproject=386
-            order by im.name,ipp.ppr_famille
-            -- limit 30;
-        """
+        #** Valeur par défaut *************************************************
+        if not type_moule:
+            type_moule='Actif'
 
 
-        cr.execute(SQL)
-        rows = cr.dictfetchall()
+        print(cp_id,type(cp_id),client,type(client))
 
+
+        #** Liste de choix cp_options *****************************************
+        cp_options=[]
+        cp_options.append({
+            "id"      : 0,
+            "name"    : '',
+            "selected": False,
+        })
+        group = self.env.ref('is_plastigray16.is_chef_projet_group')
+        for user in group.users:
+            selected=False
+            if str(user.id)==cp_id:
+                selected=True
+            cp_options.append({
+                "id"      : user.id,
+                "name"    : user.name,
+                "selected": selected,
+            })
+
+        #** Liste de choix type_moule_options *********************************
+        options = ["Actif","Tous"]
+        type_moule_options=[]
+        for o in options:
+            selected=False
+            if o==type_moule:
+                selected=True
+            type_moule_options.append({
+                "id": o,
+                "name": o,
+                "selected": selected,
+            })
+
+        #** Liste de choix modele_options *************************************
+        modele_options=[]
+        lines=self.env['is.modele.bilan'].search([],order='mb_titre')
+        for line in lines:
+            selected=False
+            if str(line.id)==modele_id:
+                selected=True
+            modele_options.append({
+                "id"      : line.id,
+                "name"    : line.mb_titre,
+                "selected": selected,
+            })
+        #**********************************************************************
+
+
+        #** Recherche des familles du modele **********************************
+        modele_ids=[]
+        if modele_id:
+            modeles=self.env['is.modele.bilan'].search([('id','=',modele_id)])
+
+            for modele in modeles:
+                print(modele.line_ids)
+                for line in modele.line_ids:
+                    print(line.param_project_id.ppr_famille)
+                    id = line.param_project_id.id
+                    if id:
+                        modele_ids.append(str(id))
+
+        print(modele_ids)
         mydict={}
-        for row in rows:
-            print(row['id'])
-            idmoule          = row['idmoule']
-
-            key="%s-%s"%(row['moule'],idmoule)
-
-            param_project_id = row['param_project_id']
-
-            if key not in mydict:
-                vals={
-                    'key'    : key,
-                    'idmoule': idmoule,
-                    'moule'  : row['moule'],
-                }
-                mydict[key]=vals
 
 
-
-            # if param_project_id not in mydict[idmoule]:
-            #     mydict[idmoule][param_project_id]=row
+        if len(modele_ids)>0:
 
 
+            SQL="""
+                select
+                    idm.id,
+                    im.name         moule,
+                    idm.idmoule,
+                    im.project,
+                    imp.name        projet,
+                    imp.chef_projet_id,
+                    rp.name         cp,
+                    imp.client_id,
+                    client.name     client,
+                    ipp.ppr_famille famille,
+                    idm.param_project_id
+                from is_doc_moule idm inner join is_mold im           on idm.idmoule=im.id
+                                    inner join is_mold_project imp  on im.project=imp.id
+                                    inner join res_users ru         on imp.chef_projet_id=ru.id
+                                    inner join res_partner rp       on ru.partner_id=rp.id
+                                    inner join res_partner client   on imp.client_id=client.id
+                                    inner join is_param_project ipp on idm.param_project_id=ipp.id
+                where idm.active='t' and idm.param_project_id in (%s)
+            """%','.join(modele_ids)
+            if cp_id and cp_id!='0':
+                SQL+=" and ru.id=%s "%cp_id
+            if client:
+                SQL+=" and client.name ilike '%"+client+"%' "
+            if projet:
+                SQL+=" and imp.name ilike '%"+projet+"%' "
+            if moule:
+                SQL+=" and im.name ilike '"+moule+"%' "
+            if type_moule and type_moule=='Actif':
+                SQL+=" and im.date_fin is null "
 
-        print(mydict)
+
+            SQL+="""
+                order by im.name,ipp.ppr_famille
+                limit 1000;
+            """
+            print(SQL)
+
+            cr.execute(SQL)
+            rows = cr.dictfetchall()
+
+            for row in rows:
+
+                #print(row)
+
+                idmoule          = row['idmoule']
+                key="%s-%s"%(row['moule'],idmoule)
+                param_project_id = row['param_project_id']
+
+                if key not in mydict:
+                    vals={
+                        'key'    : key,
+                        'idmoule': idmoule,
+                        'moule'  : row['moule'],
+                    }
+                    mydict[key]=vals
 
 
-        #sorted_dict = dict(sorted(mydict.items())) 
-
-
-
-        print(cp,client,projet, moule,type_moule,modele,ok)
-        #lines=self.env['is.doc.moule'].search([], limit=10) #, order="dateend"
         res={
-            "cp"        : cp,
-            "client"    : client,
-            "projet"    : projet,
-            "moule"     : moule,
-            "type_moule": type_moule,
-            "modele"    : modele,
-            "dict"      : mydict,
+            "dict"              : mydict,
+            "client"            : client,
+            "projet"            : projet,
+            "moule"             : moule,
+            "cp_id"             : cp_id,
+            "cp_options"        : cp_options,
+            "type_moule"        : type_moule,
+            "type_moule_options": type_moule_options,
+            "modele_id"         : modele_id,
+            "modele_options"    : modele_options,
         }
         return res
 
--- static/src/suivi_projet/suivi_projet.js
+++ static/src/suivi_projet/suivi_projet.js
@@ -36,9 +36,18 @@ class SuiviProjet extends Component {
         this.getSuiviProjet(true);
     }
 
+    // onChangeInput(ev) {
+    //     this.state[ev.target.name] = ev.target.value;
+    // }
+
+
+
     onChangeInput(ev) {
+        console.log("onChangeInput",ev.target.name,ev.target.value);
         this.state[ev.target.name] = ev.target.value;
     }
+
+
 
     OKkey(ev) {
         if (ev.keyCode === 13) {
@@ -78,36 +87,29 @@ class SuiviProjet extends Component {
     }
 
     async getSuiviProjet(ok=false){
-
-
-        console.log('TEST 2');
-
-        // <td><input name="suivi_projet_cp"         t-att-value="state.suivi_projet_cp"         t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-        // <td><input name="suivi_projet_client"     t-att-value="state.suivi_projet_client"     t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-        // <td><input name="suivi_projet_projet"     t-att-value="state.suivi_projet_projet"     t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-        // <td><input name="suivi_projet_moule"      t-att-value="state.suivi_projet_moule"      t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-        // <td><input name="suivi_projet_type_moule" t-att-value="state.suivi_projet_type_moule" t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-        // <td><input name="suivi_projet_modele"     t-att-value="state.suivi_projet_modele"     t-on-change="onChangeInput" t-on-keyup="OKkey" type="text" class="form-control"/></td>
-
-
-
         const params={
-            "cp"         : this.state.suivi_projet_cp,        
+            "cp_id"      : this.state.suivi_projet_cp_id,        
             "client"     : this.state.suivi_projet_client,        
             "projet"     : this.state.suivi_projet_projet,        
             "moule"      : this.state.suivi_projet_moule,        
             "type_moule" : this.state.suivi_projet_type_moule,        
-            "modele"     : this.state.suivi_projet_modele,        
+            "modele_id"  : this.state.suivi_projet_modele_id,        
             "ok"         : ok,
         }
         var res = await this.orm.call("is.doc.moule", 'get_suivi_projet', [false],params);
-        this.state.suivi_projet_cp         = res.cp;
+        this.state.dict                    = res.dict;
         this.state.suivi_projet_client     = res.client;
         this.state.suivi_projet_projet     = res.projet;
         this.state.suivi_projet_moule      = res.moule;
+
+        this.state.suivi_projet_cp_id      = res.cp_id;
+        this.state.cp_options              = res.cp_options;
+
         this.state.suivi_projet_type_moule = res.type_moule;
-        this.state.suivi_projet_modele     = res.modele;
-        this.state.dict  = res.dict;
+        this.state.type_moule_options      = res.type_moule_options;
+
+        this.state.suivi_projet_modele_id  = res.modele_id;
+        this.state.modele_options          = res.modele_options;
     }
 }
 
