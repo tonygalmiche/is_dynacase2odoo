881dce07028d42bfe1e45bc1e8c5ab01551ae22e
Tony Galmiche <tony.galmiche@infosaone.com>
2024-10-21 15:56:16 +0200

    modifs

--- models/is_creation_doc_migration.py
+++ models/is_creation_doc_migration.py
@@ -34,16 +34,66 @@ class is_creation_doc_migration(models.Model):
                 ('idmoule','=', obj.moule_id.id)
             ]
             lines=self.env['is.doc.moule'].search(domain)
+
+            #TODO : 
+            #- Si J prévu n'est pas renseigné et si un esule documment à faire => J prévue = J indiquée dans le paramètrage
+            #- Si J prévue non renseignée et plusieurs documents à faire => La premiere J indiquée dans le paramétgrage
+            # Il faudrait donc commencer par initialiser les J prévue de tous les documents pour connaitre les documents à créer
+
+            #** Recherche du nombre de documents pas famille ******************
+            familles={}
+            for line in lines:
+                famille_id = line.param_project_id.id
+                if famille_id not in familles:
+                    familles[famille_id]=0
+                familles[famille_id]+=1
+
             analyse=[]
             j_actuelle=dict(GESTION_J).get(obj.moule_id.j_actuelle)
             analyse.append('%s documents actuellement'%len(lines))
             analyse.append('J actuelle : %s'%j_actuelle)
+
+
+            nb_a_creer_total=0
             for line in lines:
+                print(line.param_project_id.ppr_famille)
+
+
+                famille_id = line.param_project_id.id
+                nb_doc_famille = familles.get(famille_id)
                 j_prevue=str(dict(GESTION_J).get(line.j_prevue)).ljust(15)
 
 
-                analyse.append('- %s : %s'%(j_prevue,line.param_project_id.ppr_famille))
+                #** Recherche des documents à créer après la J actuelle *******
+                if nb_doc_famille==1:
+                    for param in line.param_project_id.array_ids:
+                        if param.ppp_j>obj.moule_id.j_actuelle and param.ppr_irv:
+                            nb_a_creer_total+=1
+                            #print('-',obj.moule_id.j_actuelle, param.ppp_j, param.ppr_irv)
 
+
+
+                            analyse.append('- %s : nb_doc=%s : J prévue=%s : %s'%(
+                                j_prevue,
+                                str(nb_doc_famille).ljust(2),
+                                param.ppp_j,
+                                line.param_project_id.ppr_famille[:48].ljust(50))
+                            )
+
+
+
+
+                #**************************************************************
+
+                # if nb_a_creer>0:
+                #     analyse.append('- %s : nb_doc=%s : nb_a_creer=%s : %s'%(
+                #         j_prevue,
+                #         str(nb_doc_famille).ljust(2),
+                #         str(nb_a_creer).ljust(2),
+                #         line.param_project_id.ppr_famille[:48].ljust(50))
+                #     )
+
+            analyse.append('%s documents à créer au total'%nb_a_creer_total)
 
 
             obj.analyse = '\n'.join(analyse)
