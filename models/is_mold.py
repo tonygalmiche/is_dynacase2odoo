6b6273941f898f184aebe5fe59742c6172d314a1
Tony Galmiche <tony.galmiche@infosaone.com>
2024-11-08 09:37:33 +0100

    modifs

--- models/is_mold.py
+++ models/is_mold.py
@@ -5,13 +5,14 @@ from odoo.addons.is_dynacase2odoo.models.is_param_project import GESTION_J, TYPE
 class is_mold(models.Model):
     _inherit = 'is.mold'
 
-    image              = fields.Binary('Image')
-    revue_contrat_id   = fields.Many2one("is.revue.de.contrat", string="Revue de contrat"  , copy=False)
-    revue_lancement_id = fields.Many2one("is.revue.lancement" , string="Revue de lancement", copy=False)
-    revue_risque_id    = fields.Many2one("is.revue.risque"    , string="Revue des risques" , copy=False)
-    j_actuelle         = fields.Selection(GESTION_J, string="J Actuelle", tracking=True    , copy=False)
-    j_avancement       = fields.Integer(string="Avancement J (%)"       , tracking=True    , copy=False)
-    date_fin_be        = fields.Date(string="Date fin BE"               , tracking=True    , copy=False)
+    image                  = fields.Binary('Image')
+    dossier_appel_offre_id = fields.Many2one("is.dossier.appel.offre", string="Dossier appel d'offre", copy=False)
+    revue_contrat_id       = fields.Many2one("is.revue.de.contrat"   , string="Revue de contrat"     , copy=False)
+    revue_lancement_id     = fields.Many2one("is.revue.lancement"    , string="Revue de lancement"   , copy=False)
+    revue_risque_id        = fields.Many2one("is.revue.risque"       , string="Revue des risques"    , copy=False)
+    j_actuelle             = fields.Selection(GESTION_J, string="J Actuelle", tracking=True          , copy=False)
+    j_avancement           = fields.Integer(string="Avancement J (%)"       , tracking=True          , copy=False)
+    date_fin_be            = fields.Date(string="Date fin BE"               , tracking=True          , copy=False)
 
 
     def gantt_action(self):
