6f4832eded490a828aaee14e24e4ecd3856febc0
Tony Galmiche <tony.galmiche@infosaone.com>
2025-02-08 10:33:37 +0100

    odifs

--- models/is_dossier_article.py
+++ models/is_dossier_article.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models,fields,api
+from odoo import models,fields,api  # type: ignore
 
 class is_dossier_article(models.Model):
     _inherit = 'is.dossier.article'
