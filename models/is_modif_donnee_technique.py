92743ab1481d0f2559374600f072597f002df09e
Emmanuel Garette <egarette@silique.fr>
2025-03-04 14:51:53 +0100

    numero de demande

--- models/is_modif_donnee_technique.py
+++ models/is_modif_donnee_technique.py
@@ -1,13 +1,13 @@
-from odoo import models, fields  # type: ignore
+from odoo import models, fields, api  # type: ignore
 
 class is_modif_donnee_technique(models.Model):
     _name='is.modif.donnee.technique'
     _inherit     = ["portal.mixin", "mail.thread", "mail.activity.mixin", "utm.mixin"]
-    _description="Fiche information prospect"
-#    _rec_name = "nom_prospect"
-#    _order='nom_prospect'
+    _description="Demande modification donnée technique"
+    _rec_name = "num_demande"
+    _order='num_demande'
 
-    num_demande                   = fields.Integer("N° Demande", tracking=True)
+    num_demande                   = fields.Integer("N° Demande", readonly=True)
     active                        = fields.Boolean('Actif', default=True, tracking=True)
     moule_id                      = fields.Many2one('is.mold', 'Moule', tracking=True)
     dossierf_id                   = fields.Many2one("is.dossierf", string="Dossier F", tracking=True)
@@ -19,12 +19,11 @@ class is_modif_donnee_technique(models.Model):
 
     article                       = fields.Boolean("Modification Article", tracking=True)
     article_commentaire           = fields.Text("Commentaire article", tracking=True)
-    article_piece_jointe          = fields.Many2one("ir.attachment", "Pièce jointe article", tracking=True)
-    article_piece_jointe_ids      = fields.Many2many("ir.attachment", "is_article_piece_jointe_rel", "piece_jointe", "att_id", string="Pièce jointe article")
+    article_piece_jointe_ids      = fields.Many2many("ir.attachment", "is_modif_donnee_technique_article_piece_jointe_rel", "piece_jointe", "att_id", string="Pièce jointe article")
 
     nomenclature                  = fields.Boolean("Modification Nomenclature", tracking=True)
     nomenclature_commentaire      = fields.Text("Commentaire nomenclature", tracking=True)
-    nomenclature_piece_jointe_ids = fields.Many2many("ir.attachment", "is_nomenclature_piece_jointe_rel", "piece_jointe", "att_id", string="Pièce jointe nomenclature")
+    nomenclature_piece_jointe_ids = fields.Many2many("ir.attachment", "is_modif_donnee_technique_nomenclature_piece_jointe_rel", "piece_jointe", "att_id", string="Pièce jointe nomenclature")
 
     gamme_article                 = fields.Boolean("Modification Gamme", tracking=True)
     gamme_commentaire             = fields.Text("Commentaire", tracking=True)
@@ -39,3 +38,15 @@ class is_modif_donnee_technique(models.Model):
                 'url': url,
                 'target': 'new',
             }
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if "num_demande" not in vals:
+                last_codif = self.env["is.modif.donnee.technique"].search([('num_demande', '!=', None)], order="num_demande desc", limit=1)
+                if last_codif:
+                    num_demande = last_codif.num_demande
+                else:
+                    num_demande = -1
+                vals["num_demande"] = num_demande + 1
+        return super().create(vals_list)
