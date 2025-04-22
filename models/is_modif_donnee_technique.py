a52f04efd0390e72bf5d119796a877614d374f81
Tony Galmiche <tony.galmiche@infosaone.com>
2025-04-22 12:59:36 +0200

    modifs

--- models/is_modif_donnee_technique.py
+++ models/is_modif_donnee_technique.py
@@ -40,7 +40,48 @@ class is_modif_donnee_technique(models.Model):
     vers_termine_vsb              = fields.Boolean(string="vers Terminé"          , compute='_compute_vsb', readonly=True, store=False)
     vers_refuse_vsb               = fields.Boolean(string="vers Refusé"           , compute='_compute_vsb', readonly=True, store=False)
     readonly_vsb                  = fields.Boolean(string="Accès en lecture seule", compute='_compute_vsb', readonly=True, store=False)
+    mail_to_ids   = fields.Many2many('res.users', compute='_compute_mail_to_cc_ids', string="Mail To")
+    mail_cc_ids   = fields.Many2many('res.users', compute='_compute_mail_to_cc_ids', string="Mail Cc")
 
+
+    def get_state_name(self):
+        for obj in self:
+            return dict(self._fields['state'].selection).get(self.state)
+
+
+    @api.depends("state")
+    def _compute_mail_to_cc_ids(self):
+        user = self.env['res.users'].browse(self._uid)
+        for obj in self:
+            to_ids=[]
+            cc_ids=[]
+            if obj.state=='mdt_diffuse':
+                to_ids.append(obj.responsable_action)
+            if obj.state=='mdt_termine':
+                to_ids.append(obj.demandeur)
+            if obj.state=='mdt_refuse':
+                to_ids.append(obj.demandeur)
+            obj.mail_to_ids = self.env['is.liste.diffusion.mail'].get_users_ids(users=to_ids)
+            obj.mail_cc_ids = self.env['is.liste.diffusion.mail'].get_users_ids(users=cc_ids)
+
+
+    def users2mail(self,users):
+        return self.env['is.liste.diffusion.mail'].users2mail(users=users)
+      
+
+    def users2partner_ids(self,users):
+        return self.env['is.liste.diffusion.mail'].users2partner_ids(users=users)
+
+
+    def envoi_mail(self):
+        template = self.env.ref('is_dynacase2odoo.is_modif_donnee_technique_mail_template').sudo()     
+        email_values = {
+            'email_cc'      : self.users2mail(self.mail_cc_ids),
+            'auto_delete'   : False,
+            'recipient_ids' : self.users2partner_ids(self.mail_to_ids),
+            'scheduled_date': False,
+        }
+        template.send_mail(self.id, force_send=True, raise_exception=False, email_values=email_values)
 
 
     @api.depends("state")
@@ -81,14 +122,17 @@ class is_modif_donnee_technique(models.Model):
     def vers_diffuse_action(self):
         for obj in self:
             obj.state='mdt_diffuse'
+            obj.envoi_mail()
 
     def vers_termine_action(self):
         for obj in self:
             obj.state='mdt_termine'
+            obj.envoi_mail()
 
     def vers_refuse_action(self):
         for obj in self:
             obj.state='mdt_refuse'
+            obj.envoi_mail()
 
 
     def lien_vers_dynacase_action(self):
