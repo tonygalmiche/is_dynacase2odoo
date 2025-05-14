305db1cb31f30d07bac4196c0d03d177eae7f60b
Tony Galmiche <tony.galmiche@infosaone.com>
2025-05-14 11:24:36 +0200

    modifs

--- models/is_prise_avance.py
+++ models/is_prise_avance.py
@@ -12,7 +12,7 @@ class is_prise_avance(models.Model):
     _inherit     = ["portal.mixin", "mail.thread", "mail.activity.mixin", "utm.mixin"]
     _description="Prise d'avance"
     _rec_name = "num_moule_id"
-    _order='num_moule_id'
+    _order='create_date desc'
 
     vers_brouillon_vsb          = fields.Boolean('vers_brouillon_vsb', compute='_compute_vsb', readonly=True, store=False)
     vers_diffuse_vsb            = fields.Boolean('vers_diffuse_vsb', compute='_compute_vsb', readonly=True, store=False)
@@ -50,6 +50,52 @@ class is_prise_avance(models.Model):
     date_retour_outillage_vsb   = fields.Boolean("date_retour_outillage_vsb", compute="_compute_vsb", readonly=True, store=False)
     state                       = fields.Selection(_STATE, "Etat", default=_STATE[0][0], required=True, tracking=True)
     dynacase_id                 = fields.Integer(string="Id Dynacase", index=True, copy=False)
+    mail_to_ids                 = fields.Many2many('res.users', compute='_compute_mail_to_cc_ids', string="Mail To")
+    mail_cc_ids                 = fields.Many2many('res.users', compute='_compute_mail_to_cc_ids', string="Mail Cc")
+
+
+    def get_doc_url(self):
+        for obj in self:
+            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+            url = base_url + '/web#id=%s' '&view_type=form&model=%s'%(obj.id,self._name)
+            return url
+
+
+    def get_state_name(self):
+        for obj in self:
+            return dict(self._fields['state'].selection).get(self.state)
+
+
+    @api.depends("state")
+    def _compute_mail_to_cc_ids(self):
+        for obj in self:
+            to_ids=[]
+            if obj.state=='diffuse':
+                to_ids.append(obj.resp_prise_avance_id)
+            if obj.state=='realise':
+                to_ids.append(obj.user_id)
+            obj.mail_to_ids = self.env['is.liste.diffusion.mail'].get_users_ids(users=to_ids)
+            obj.mail_cc_ids = []
+
+
+    def users2mail(self,users):
+        return self.env['is.liste.diffusion.mail'].users2mail(users=users)
+      
+
+    def users2partner_ids(self,users):
+        return self.env['is.liste.diffusion.mail'].users2partner_ids(users=users)
+
+
+    def envoi_mail(self):
+        template = self.env.ref('is_dynacase2odoo.is_prise_avance_mail_template').sudo()     
+        email_values = {
+            'email_cc'      : self.users2mail(self.mail_cc_ids),
+            'auto_delete'   : False,
+            'recipient_ids' : self.users2partner_ids(self.mail_to_ids),
+            'scheduled_date': False,
+        }
+        template.send_mail(self.id, force_send=True, raise_exception=False, email_values=email_values)
+
 
     @api.depends("state")
     def _compute_ro(self):
@@ -138,10 +184,12 @@ class is_prise_avance(models.Model):
     def vers_diffuse_action(self):
         for obj in self:
             obj.state='diffuse'
+            obj.envoi_mail()
 
     def vers_realise_action(self):
         for obj in self:
             obj.state='realise'
+            obj.envoi_mail()
 
     def vers_brouillon_action(self):
         for obj in self:
