5cd00f5896e2c3a14e969f219d872916e4a2b5ff
Tony Galmiche <tony.galmiche@infosaone.com>
2025-05-25 13:31:21 +0200

    modifs

--- models/is_facture_outillage.py
+++ models/is_facture_outillage.py
@@ -45,7 +45,7 @@ class is_facture_outillage(models.Model):
     moule_id                  = fields.Many2one("is.mold"                  , string="Moule"                   , tracking=True)
     dossierf_id               = fields.Many2one("is.dossierf"              , string="Dossier F"               , tracking=True)
     erd_id                    = fields.Many2one("is.erd"                   , string="ERD"                     , tracking=True)
-    client_id                 = fields.Many2one("res.partner"              , string="Client"                  , tracking=True)
+    client_id                 = fields.Many2one("res.partner", string="Client", tracking=True, domain=[("is_company","=",True), ("customer","=",True)])
     client_autre              = fields.Char("Client (autre)"                                                  , tracking=True)
     commercial_id             = fields.Many2one("res.users"                , string="Commercial"              , tracking=True, compute="_compute_designation", store=True, readonly=True)
     chef_projet_id            = fields.Many2one("res.users"                , string="Chef de projet"          , tracking=True, compute="_compute_designation", store=True, readonly=True)
@@ -77,32 +77,13 @@ class is_facture_outillage(models.Model):
             montant_ht=[]
             date_facture_prev=[]
             date_facture=[]
-            date_reglement=[]
-            
-            # html='<table style="width:400px">'
+            date_reglement=[]            
             for line in obj.ligne_ids:
                 num_facture.append(line.num_facture or '')
                 montant_ht.append('{0:,.2f}'.format(line.montant_ht).replace(',',' ').replace('.',','))
                 date_facture_prev.append((line.date_facture_prev and line.date_facture_prev.strftime('%d/%m/%Y')) or '')
                 date_facture.append((line.date_facture and line.date_facture.strftime('%d/%m/%Y')) or '')
                 date_reglement.append((line.date_reglement and line.date_reglement.strftime('%d/%m/%Y')) or '')
-            #     html+="""
-            #         <tr>
-            #             <td style="width:20%%;border:1px;text-align:left">%s</td>
-            #             <td style="width:20%%;border:1px;text-align:right">%s</td>
-            #             <td style="width:20%%;border:1px;text-align:center">%s</td>
-            #             <td style="width:20%%;border:1px;text-align:center">%s</td>
-            #             <td style="width:20%%;border:1px;text-align:center">%s</td>
-            #         </tr>
-            #     """%(
-            #         line.num_facture or '',
-            #         '{0:,.2f}'.format(line.montant_ht).replace(',',' ').replace('.',','),
-            #         (line.date_facture_prev and line.date_facture_prev.strftime('%d/%m/%Y')) or '',
-            #         (line.date_facture      and line.date_facture.strftime('%d/%m/%Y')) or '',
-            #         (line.date_reglement    and line.date_reglement.strftime('%d/%m/%Y')) or '',
-            #     )
-            # html+='</table>'
-            # obj.ligne_html = html
             obj.num_facture = '\n'.join(num_facture)
             obj.montant_ht = '\n'.join(montant_ht)
             obj.date_facture_prev = '\n'.join(date_facture_prev)
@@ -192,46 +173,35 @@ class is_facture_outillage_ligne(models.Model):
     commentaire       = fields.Text("Commentaire")
    
 
-    # @api.onchange('num_facture')
-    # def actualiser_ligne_action(self):
-    #     for obj in self:
-    #         res = obj.get_values()
-    #         if len(res)==6:
-    #             obj.montant_ht      = res[0]
-    #             obj.montant_ttc     = res[1]
-    #             obj.montant_paye_ht = res[2]
-    #             obj.date_facture    = res[3]
-    #             obj.date_echeance   = res[4]
-    #             obj.date_reglement  = res[5]
+    def voir_facture_action(self):
+        for obj in self:
+            cr_odoo1,test = self.get_cr_odoo1()
+            url=False
+            if obj.type_facture in ['Facture','Avoir']:
+                SQL="SELECT id from account_move where name=%s"
+                cr_odoo1.execute(SQL,[obj.num_facture])
+                rows = cr_odoo1.fetchall()
+                for row in rows:    
+                    url="https://odoo1/web#id=%s&model=account.move&view_type=form"%row['id']
+            if obj.type_facture in ['Proforma']:
+                SQL="SELECT id from is_facture_proforma_outillage where name=%s"
+                cr_odoo1.execute(SQL,[obj.num_facture])
+                rows = cr_odoo1.fetchall()
+                for row in rows:    
+                    url="https://odoo1/web#id=%s&model=is.facture.proforma.outillage&view_type=form"%row['id']
+            if url:
+                return {
+                    'type' : 'ir.actions.act_url',
+                    'url': url,
+                    'target': 'new',
+                }
 
 
-    # def get_values(self):
-    #     for obj in self:
-    #         res=[]
-    #         if obj.num_facture:
-    #             name = "actualiser-facture-outillage"
-    #             cdes = self.env['is.commande.externe'].search([('name','=',name)])
-    #             if len(cdes)==0:
-    #                 raise ValidationError("Commande externe '%s' non trouvée !"%name)
-    #             for cde in cdes:
-    #                 commande = cde.commande.replace("#type_facture", obj.type_facture)
-    #                 commande = commande.replace("#num_facture" , obj.num_facture)
-    #                 p = Popen(commande, shell=True, stdout=PIPE, stderr=PIPE)
-    #                 stdout, stderr = p.communicate()
-    #                 _logger.info("%s => %s"%(commande,stdout))
-    #                 if stderr:
-    #                     raise ValidationError("Erreur dans commande externe '%s' => %s"%(commande,stderr))
-    #                 res = stdout.decode("utf-8").strip().split('|')
-    #         return res
-            
-
-    @api.onchange('num_facture')
-    def actualiser_ligne_action(self):
-        cr_cegid = cr_odoo1 = False
+    def get_cr_odoo1(self):
+        cr_odoo1 = False
         company = self.env.user.company_id
-
-        #** Connexion à Odoo 1 ************************************************
         databases=self.env['is.database'].search([('name','=', 'Gray')])
+        test=True
         for database in databases:
             host     = company.is_postgres_host or ''
             dbname   = database.database        or ''
@@ -244,7 +214,18 @@ class is_facture_outillage_ligne(models.Model):
                 msg="La connexion à Odoo1 sur la base '%s' et le serveur '%s' a échouée !"%(dbname,host)
                 msg="%s\n[%s] %s"%(msg,type(err),err)
                 _logger.error(msg)
-                raise ValidationError(msg)
+                test=False
+        return cr_odoo1,test
+
+
+
+    @api.onchange('num_facture')
+    def actualiser_ligne_action(self):
+        cr_cegid = False
+        company = self.env.user.company_id
+
+        #** Connexion à Odoo 1 ************************************************
+        cr_odoo1,test = self.get_cr_odoo1()
         #**********************************************************************
 
         ##** Connexion à CEGID ************************************************
@@ -265,108 +246,110 @@ class is_facture_outillage_ligne(models.Model):
             msg="La connexion à CEGID sur la base '%s' et le serveur '%s' a échouée !"%(DATABASE,SERVER)
             msg="%s\n[%s] %s"%(msg,type(err),err)
             _logger.error(msg)
-            raise ValidationError(msg)
+            test=False
+            #raise ValidationError(msg)
         #**********************************************************************
 
-        for obj in self:
-            type_facture = obj.type_facture
-            num_facture  = obj.num_facture
-            montant_ht = montant_ttc = montant_paye_ht = 0
-            date_facture = date_reglement = date_echeance = ''
-            if type_facture in ('Facture','Avoir'):
-               
-                if type_facture=="Facture":
-                    move_type='out_invoice'
-                if type_facture=="Avoir":
-                    move_type = 'out_refund'
-                SQL="""
-                    select id,name,amount_untaxed_signed,amount_total_signed 
-                    from account_move 
-                    where state='posted' and move_type=%s and name=%s
-                    order by name desc limit 1
-                """
-                cr_odoo1.execute(SQL,[move_type,num_facture])
-                rows = cr_odoo1.fetchall()
-                for row in rows:                
-                    montant_ht  = row['amount_untaxed_signed']
-                    montant_ttc = row['amount_total_signed']
-                    
-                    #** Recherche Date Facture dans CEGID **************************************
+        if test:
+            for obj in self:
+                type_facture = obj.type_facture
+                num_facture  = obj.num_facture
+                montant_ht = montant_ttc = montant_paye_ht = 0
+                date_facture = date_reglement = date_echeance = ''
+                if type_facture in ('Facture','Avoir'):
+                
+                    if type_facture=="Facture":
+                        move_type='out_invoice'
+                    if type_facture=="Avoir":
+                        move_type = 'out_refund'
                     SQL="""
-                        select E_DATECOMPTABLE
-                        from ECRITURE
-                        where 
-                            E_GENERAL='411000' and
-                            E_JOURNAL='VTE' and 
-                            E_REFINTERNE='%s'
-                    """%num_facture
-                    rows_cegid = cr_cegid.execute(SQL)
-                    for row_cegid in rows_cegid:
-                        date_facture = row_cegid[0]
-                    #***************************************************************************
+                        select id,name,amount_untaxed_signed,amount_total_signed 
+                        from account_move 
+                        where state='posted' and move_type=%s and name=%s
+                        order by name desc limit 1
+                    """
+                    cr_odoo1.execute(SQL,[move_type,num_facture])
+                    rows = cr_odoo1.fetchall()
+                    for row in rows:                
+                        montant_ht  = row['amount_untaxed_signed']
+                        montant_ttc = row['amount_total_signed']
+                        
+                        #** Recherche Date Facture dans CEGID **************************************
+                        SQL="""
+                            select E_DATECOMPTABLE
+                            from ECRITURE
+                            where 
+                                E_GENERAL='411000' and
+                                E_JOURNAL='VTE' and 
+                                E_REFINTERNE='%s'
+                        """%num_facture
+                        rows_cegid = cr_cegid.execute(SQL)
+                        for row_cegid in rows_cegid:
+                            date_facture = row_cegid[0]
+                        #***************************************************************************
 
-                    #** Recherche Date Réglement dans CEGID ************************************
+                        #** Recherche Date Réglement dans CEGID ************************************
+                        SQL="""
+                            select E_DATECOMPTABLE
+                            from ECRITURE
+                            where 
+                                E_GENERAL='411000' and
+                                E_JOURNAL!='VTE' and 
+                                E_REFINTERNE='%s' and
+                                E_DATECOMPTABLE>'2024-01-01'
+                        """%num_facture
+                        rows_cegid = cr_cegid.execute(SQL)
+                        for row_cegid in rows_cegid:
+                            date_reglement = row_cegid[0]
+                        #***************************************************************************
+
+                        #** Recherche Date Echéance dans CEGID *************************************
+                        SQL="""
+                            select E_DATPER
+                            from ECRITURE
+                            where 
+                                E_GENERAL='411000' and
+                                E_JOURNAL!='VTE' and 
+                                E_REFINTERNE='%s'
+                        """%num_facture
+                        rows_cegid = cr_cegid.execute(SQL)
+                        for row_cegid in rows_cegid:
+                            date_echeance = row_cegid[0]
+                        #***************************************************************************
+
+                        #** Montant payé CEGID *****************************************************
+                        SQL="""
+                            select E_CREDIT,E_GENERAL
+                            from ECRITURE
+                            where 
+                                E_JOURNAL<>'VTE' and 
+                                E_REFINTERNE='%s' and 
+                                E_AUXILIAIRE>='C500000' and 
+                                E_AUXILIAIRE<='C509999'
+                        """%num_facture
+                        rows_cegid = cr_cegid.execute(SQL)
+                        for row_cegid in rows_cegid:
+                            montant_paye_ht += row_cegid[0] / 10000
+                        #***************************************************************************
+
+                if type_facture=='Proforma':
                     SQL="""
-                        select E_DATECOMPTABLE
-                        from ECRITURE
-                        where 
-                            E_GENERAL='411000' and
-                            E_JOURNAL!='VTE' and 
-                            E_REFINTERNE='%s' and
-                            E_DATECOMPTABLE>'2024-01-01'
-                    """%num_facture
-                    rows_cegid = cr_cegid.execute(SQL)
-                    for row_cegid in rows_cegid:
-                        date_reglement = row_cegid[0]
-                    #***************************************************************************
+                        select e.id,e.name,e.num_cde, e.date_facture, sum(l.total) total
+                        from is_facture_proforma_outillage e join is_facture_proforma_outillage_line l on e.id=l.proforma_id
+                        where e.name=%s
+                        group by e.name, e.id,e.num_cde, e.date_facture 
+                        order by e.name desc limit 1
+                    """
+                    cr_odoo1.execute(SQL,[num_facture])
+                    rows = cr_odoo1.fetchall()
+                    for row in rows:
+                        montant_ht   = row['total'];
+                        montant_ttc  = row['total'];
+                        date_facture = row['date_facture']
 
-                    #** Recherche Date Echéance dans CEGID *************************************
-                    SQL="""
-                        select E_DATPER
-                        from ECRITURE
-                        where 
-                            E_GENERAL='411000' and
-                            E_JOURNAL!='VTE' and 
-                            E_REFINTERNE='%s'
-                    """%num_facture
-                    rows_cegid = cr_cegid.execute(SQL)
-                    for row_cegid in rows_cegid:
-                        date_echeance = row_cegid[0]
-                    #***************************************************************************
-
-                    #** Montant payé CEGID *****************************************************
-                    SQL="""
-                        select E_CREDIT,E_GENERAL
-                        from ECRITURE
-                        where 
-                            E_JOURNAL<>'VTE' and 
-                            E_REFINTERNE='%s' and 
-                            E_AUXILIAIRE>='C500000' and 
-                            E_AUXILIAIRE<='C509999'
-                    """%num_facture
-                    rows_cegid = cr_cegid.execute(SQL)
-                    for row_cegid in rows_cegid:
-                        montant_paye_ht += row_cegid[0] / 10000
-                    #***************************************************************************
-
-            if type_facture=='Proforma':
-                SQL="""
-                    select e.id,e.name,e.num_cde, e.date_facture, sum(l.total) total
-                    from is_facture_proforma_outillage e join is_facture_proforma_outillage_line l on e.id=l.proforma_id
-                    where e.name=%s
-                    group by e.name, e.id,e.num_cde, e.date_facture 
-                    order by e.name desc limit 1
-                """
-                cr_odoo1.execute(SQL,[num_facture])
-                rows = cr_odoo1.fetchall()
-                for row in rows:
-                    montant_ht   = row['total'];
-                    montant_ttc  = row['total'];
-                    date_facture = row['date_facture']
-
-            obj.montant_ht      = montant_ht
-            obj.montant_ttc     = montant_ttc
-            obj.montant_paye_ht = montant_paye_ht
-            obj.date_facture    = date_facture
-            obj.date_echeance   = date_echeance
-            obj.date_reglement  = date_reglement
+                obj.montant_ht      = montant_ht
+                obj.montant_ttc     = montant_ttc
+                obj.montant_paye_ht = montant_paye_ht
+                obj.date_facture    = date_facture
+                obj.date_echeance   = date_echeance
+                obj.date_reglement  = date_reglement
