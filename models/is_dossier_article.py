70f282a0206a3b9c915065e155929900d47ef9ad
Tony Galmiche <tony.galmiche@infosaone.com>
2025-10-28 08:15:52 +0100

    modifs

--- models/is_dossier_article.py
+++ models/is_dossier_article.py
@@ -11,22 +11,19 @@ class is_dossier_article(models.Model):
     nb_fait     = fields.Integer(string="Nb doc fait"   , compute='_compute_fait',store=True,readonly=True)
 
 
-    # gestionnaire = fields.Char(string="Gestionnaire", compute='_compute_gestionnaire', store=False, readonly=True)
-
-
-    # @api.depends('doc_ids', 'doc_ids.doc_id', 'doc_ids.piecejointe')
-    # def _compute_fait(self):
-    #     for obj in self:
-    #         nb_a_faire = nb_crees = nb_fait = 0
-    #         for line in obj.doc_ids:
-    #             nb_a_faire+=1
-    #             if line.doc_id:
-    #                 nb_crees+=1
-    #             if line.piecejointe:
-    #                 nb_fait+=1
-    #         obj.nb_a_faire = nb_a_faire
-    #         obj.nb_crees   = nb_crees
-    #         obj.nb_fait    = nb_fait
+    @api.depends('doc_ids', 'doc_ids.doc_id', 'doc_ids.piecejointe')
+    def _compute_fait(self):
+        for obj in self:
+            nb_a_faire = nb_crees = nb_fait = 0
+            for line in obj.doc_ids:
+                nb_a_faire+=1
+                if line.doc_id:
+                    nb_crees+=1
+                if line.piecejointe:
+                    nb_fait+=1
+            obj.nb_a_faire = nb_a_faire
+            obj.nb_crees   = nb_crees
+            obj.nb_fait    = nb_fait
 
 
     @api.depends('code_pg')
