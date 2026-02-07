ffecf673fe56dec58b77f70c73f2b45f116f02a4
Tony Galmiche <tony.galmiche@infosaone.com>
2026-02-07 10:15:04 +0100

    add report_is_oberthur_etiquette_code_barre

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import is_processus_doc_revision_wizard
 from . import is_revue_projet_jalon_refus_wizard
+from . import is_oberthur_etiquette_code_barre_wizard
---
+++ wizard/is_oberthur_etiquette_code_barre_wizard.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class IsOberthurEtiquetteCodeBarreWizard(models.TransientModel):
+    _name = 'is.oberthur.etiquette.code.barre.wizard'
+    _description = "Assistant d'impression d'étiquettes code-barres Oberthur"
+
+    reference_coque = fields.Char(
+        string="Référence de la coque 1ier Flux",
+        required=True,
+        default=lambda self: self._default_reference_coque(),
+    )
+    numero_chrono = fields.Integer(
+        string="N° de Chrono de la 1ière Étiquette",
+        required=True,
+        default=lambda self: self._default_numero_chrono(),
+    )
+    nb_etiquettes = fields.Integer(
+        string="Nombre d'étiquettes",
+        required=True,
+        default=33,
+        help="Nombre total d'étiquettes à imprimer (33 par page : 3 colonnes x 11 lignes)",
+    )
+    afficher_bordure = fields.Boolean(
+        string="Afficher les bordures des étiquettes",
+        default=False,
+    )
+    afficher_bordure_page = fields.Boolean(
+        string="Afficher la bordure de la page",
+        default=False,
+    )
+    hauteur_etiquette = fields.Float(
+        string="Hauteur étiquette (mm)",
+        required=True,
+        default=25.5,
+        help="Hauteur de chaque étiquette en mm. Pour 11 lignes sur A4 avec marges 5mm : (297-5-0-(10*5))/11 ≈ 22mm",
+    )
+
+    def _default_reference_coque(self):
+        return self.env['is.mem.var'].get(self.env.uid, 'oberthur_eti_reference_coque') or 'K0301A'
+
+    def _default_numero_chrono(self):
+        val = self.env['is.mem.var'].get(self.env.uid, 'oberthur_eti_numero_chrono')
+        return int(val) if val else 1
+
+    def action_imprimer(self):
+        """Lance l'impression des étiquettes code-barres"""
+        self.ensure_one()
+        # Mémoriser les valeurs pour la prochaine utilisation
+        mem_var = self.env['is.mem.var']
+        mem_var.set(self.env.uid, 'oberthur_eti_reference_coque', self.reference_coque)
+        mem_var.set(self.env.uid, 'oberthur_eti_numero_chrono', str(self.numero_chrono))
+        # Préparer les données des étiquettes
+        etiquettes = []
+        for i in range(self.nb_etiquettes):
+            numero = self.numero_chrono + i
+            numero_str = str(numero).zfill(5)
+            code = self.reference_coque + numero_str
+            etiquettes.append({
+                'code': code,
+                'barcode': '*' + code + '*',
+            })
+
+        # Regrouper par lignes de 3
+        lignes = []
+        for i in range(0, len(etiquettes), 3):
+            ligne = etiquettes[i:i+3]
+            # Compléter la ligne si moins de 3 étiquettes
+            while len(ligne) < 3:
+                ligne.append({'code': '', 'barcode': ''})
+            lignes.append(ligne)
+
+        # Regrouper par pages de 11 lignes
+        pages = []
+        for i in range(0, len(lignes), 11):
+            pages.append(lignes[i:i+11])
+
+        data = {
+            'pages': pages,
+            'reference_coque': self.reference_coque,
+            'numero_chrono': self.numero_chrono,
+            'afficher_bordure': self.afficher_bordure,
+            'afficher_bordure_page': self.afficher_bordure_page,
+            'hauteur_etiquette': self.hauteur_etiquette,
+        }
+        return self.env.ref('is_dynacase2odoo.is_oberthur_etiquette_code_barre_report').report_action(self, data=data)
+
+
+class IsOberthurEtiquetteCodeBarreReport(models.AbstractModel):
+    _name = 'report.is_dynacase2odoo.report_is_oberthur_etiquette_code_barre'
+    _description = "Rapport étiquettes code-barres Oberthur"
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        return {
+            'doc_ids': docids,
+            'doc_model': 'is.oberthur.etiquette.code.barre.wizard',
+            'docs': self.env['is.oberthur.etiquette.code.barre.wizard'].browse(docids),
+            'pages': data.get('pages', []) if data else [],
+            'afficher_bordure': data.get('afficher_bordure', False) if data else False,
+            'afficher_bordure_page': data.get('afficher_bordure_page', False) if data else False,
+            'hauteur_etiquette': data.get('hauteur_etiquette', 22.0) if data else 22.0,
+        }
